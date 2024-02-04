feeea1d08f1e11896d12bac4c497048a65228717
finebel <46824694+finebel@users.noreply.github.com>
2024-02-04 18:15:40 +0100

    Deliver empty FeedItems array on 200 HTTP Response with empty JSON list

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -75,11 +75,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
         )
     }
     
-//    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
-//        let (sut, client) = makeSUT()
-//        
-//
-//    }
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+        
+        var capturedResuts: [RemoteFeedLoader.Result] = []
+        sut.load { capturedResuts.append($0) }
+        
+        let emptyListJSON = Data("{\"items\": []}".utf8)
+        client.complete(withStatusCode: 200, data: emptyListJSON)
+        
+        XCTAssertEqual(capturedResuts, [.success([])])
+    }
     
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
