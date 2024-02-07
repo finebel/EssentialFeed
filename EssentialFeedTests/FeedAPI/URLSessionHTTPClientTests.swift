a1ea871c040ec2be1dccb6b10f2140475c74d0b2
finebel <46824694+finebel@users.noreply.github.com>
2024-02-07 11:39:23 +0100

    Add nil data and response arguments to failure test case to make the setup more explicit
    Finished lecture 'Four Approaches to Test(-Drive) Network Requests End-to-End, Subclass & Protocol-Based Mocking, and URLProtocol Stubbing'

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -32,7 +32,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let url = URL(string: "https://a-url.com")!
         let error = NSError(domain: "any error", code: 1)
         let sut = URLSessionHTTPClient()
-        URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
         
         let exp = expectation(description: "Completion handler is called once")
         sut.get(from: url) { result in
@@ -57,11 +57,13 @@ final class URLSessionHTTPClientTests: XCTestCase {
         private static var stubs: [URL: Stub] = [:]
         
         private struct Stub {
+            let data: Data?
+            let response: URLResponse?
             let error: Error?
         }
         
-        static func stub(url: URL, error: Error? = nil) {
-            stubs[url] = Stub(error: error)
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
+            stubs[url] = Stub(data: data, response: response, error: error)
         }
         
         static func startInterceptingRequests() {
@@ -74,7 +76,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
-            guard let url = request.url else { return true }
+            guard let url = request.url else { return false }
             
             return stubs[url] != nil
         }
@@ -85,6 +87,14 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         override func startLoading() {
             guard let url = request.url, let stub = Self.stubs[url] else { return }
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
             
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
