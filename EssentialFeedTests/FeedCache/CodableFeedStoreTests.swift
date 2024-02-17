09005d764fc23f72a9aa69b7865d76e7fec2e416
finebel <46824694+finebel@users.noreply.github.com>
2024-02-17 12:54:01 +0100

    Inserting to non-empty cache overrides previously inserted cache values

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -136,6 +136,20 @@ final class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .failure(error: anyNSError()))
     }
     
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        let sut = makeSUT()
+        
+        let firstInsertionError = insert((uniqueImageFeed().local, Date()), to: sut)
+        XCTAssertNil(firstInsertionError, "Expected to insert successfully")
+        
+        let latestFeed = uniqueImageFeed().local
+        let latestDate = Date()
+        let latestInsertionError = insert((latestFeed, latestDate), to: sut)
+        
+        XCTAssertNil(latestInsertionError, "Expected to override existing cache successfully")
+        expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestDate))
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
@@ -145,18 +159,20 @@ final class CodableFeedStoreTests: XCTestCase {
         return sut
     }
     
+    @discardableResult
     private func insert(
         _ cache: (feed: [LocalFeedImage], timestamp: Date),
-        to sut: CodableFeedStore,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
+        to sut: CodableFeedStore
+    ) -> Error? {
         let exp = expectation(description: "Wait for cache insertion")
-        sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
-            XCTAssertNil(insertionError, "Expected feed to be inserted successfully", file: file, line: line)
+        var insertionError: Error?
+        sut.insert(cache.feed, timestamp: cache.timestamp) { receivedInsertionError in
+            insertionError = receivedInsertionError
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
+        
+        return insertionError
     }
     
     private func expect(
