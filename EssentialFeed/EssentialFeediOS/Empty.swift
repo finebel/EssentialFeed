6c3c9ca6cc395d8569c5634d0437711f1a5c84b7
finebel <46824694+finebel@users.noreply.github.com>
2024-02-27 19:50:31 +0100

    Add `EssentialFeediOS` framework (prod and test) targets for iOS platform-specific components

---
+++ EssentialFeed/EssentialFeediOS/Empty.swift
@@ -0,0 +1,8 @@
+//
+//  Empty.swift
+//  EssentialFeediOS
+//
+//  Created by Finn Ebeling on 27.02.24.
+//
+
+import Foundation
---
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Finn Ebeling on 27.02.24.
+//
+
+import XCTest
+@testable import EssentialFeediOS
+
+final class EssentialFeediOSTests: XCTestCase {
+    func test_temp() {
+        XCTAssertTrue(1 == 1)
+    }
+}
