d68e7e4c20148a749be09ba8e98a538aabcd0b4f
finebel <46824694+finebel@users.noreply.github.com>
2024-03-03 16:10:18 +0100

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeed
+//
+//  Created by Finn Ebeling on 03.03.24.
+//
+
+import UIKit
+
+public class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -10,6 +10,7 @@ import EssentialFeed
 
 public final class FeedViewController: UITableViewController {
     private var loader: FeedLoader?
+    private var tableModel: [FeedImage] = []
     
     private var viewAppeared = false
     
@@ -35,9 +36,27 @@ public final class FeedViewController: UITableViewController {
     
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load() { [weak self] _ in
+        loader?.load() { [weak self] result in
             guard let self else { return }
+            tableModel = (try? result.get()) ?? []
+            
+            tableView.reloadData()
             refreshControl?.endRefreshing()
         }
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        
+        return cell
     }
 }
