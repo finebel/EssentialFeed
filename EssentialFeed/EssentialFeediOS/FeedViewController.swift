3286b5ddf6a84978b00a35b6548df5c09fbec403
finebel <46824694+finebel@users.noreply.github.com>
2024-03-04 20:51:48 +0100

    Retry failed image load on retry action

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -73,13 +73,20 @@ public final class FeedViewController: UITableViewController {
         cell.feedImageRetryButton.isHidden = true
         
         cell.feedimageContainer.startShimmering()
-        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
-            let data = try? result.get()
-            let image = data.map(UIImage.init) ?? nil
-            cell?.feedImageView.image = image
-            cell?.feedImageRetryButton.isHidden = (image != nil)
-            cell?.feedimageContainer.stopShimmering()
+        let loadImage = { [weak self, weak cell] in
+            guard let self else { return }
+            
+            tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.feedImageRetryButton.isHidden = (image != nil)
+                cell?.feedimageContainer.stopShimmering()
+            }
         }
+        
+        cell.onRetry = loadImage
+        loadImage()
         
         return cell
     }
