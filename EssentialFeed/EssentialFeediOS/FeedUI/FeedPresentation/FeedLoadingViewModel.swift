d783129bd14ddcf12be61190f90eb69277bedd57
finebel <46824694+finebel@users.noreply.github.com>
2024-03-15 21:21:42 +0100

    Move ViewModels to separate files for better separation of concerns
    Finished lecture 'MVP: Creating a Reusable and Cross-Platform Presentation Layer, Implementing Service Adapters, and Solving Cyclic Dependencies & Memory Management issues with the Proxy Pattern'

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Finn Ebeling on 15.03.24.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/FeedUI/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Finn Ebeling on 15.03.24.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
