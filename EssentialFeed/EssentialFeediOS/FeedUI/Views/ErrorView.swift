c6ccf73a76726545a83fda42befa265f6d6eeb00
finebel <46824694+finebel@users.noreply.github.com>
2024-03-29 10:18:19 +0100

    Show and Hide error message animated
    Reusing the `ErrorView` implementation of the Feed UI Challenge (=> using a button instead of an UILabel)

--- EssentialFeed/EssentialFeediOS/FeedUI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -11,12 +11,37 @@ public final class ErrorView: UIView {
     @IBOutlet private var button: UIButton!
     
     public var message: String? {
-        button.title(for: .normal)
+        isVisible ? button.title(for: .normal) : nil
+    }
+    
+    private var isVisible: Bool {
+        return alpha > 0
     }
     
     public override func awakeFromNib() {
         super.awakeFromNib()
         
         button.setTitle(nil, for: .normal)
+        alpha = 0
+    }
+    
+    func show(message: String) {
+        button.setTitle(message, for: .normal)
+
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+
+    func hideMessage() {
+        UIView.animate(
+            withDuration: 0.25,
+            animations: { self.alpha = 0 },
+            completion: { completed in
+                if completed {
+                    self.button.setTitle(nil, for: .normal)
+                }
+            }
+        )
     }
 }
