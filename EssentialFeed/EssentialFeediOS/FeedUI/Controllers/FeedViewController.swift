ec46a75d719caca0061e77a32cd8b3a3c4133acb
finebel <46824694+finebel@users.noreply.github.com>
2024-03-29 10:32:50 +0100

    Add reusable refresh control helper extension

--- EssentialFeed/EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
@@ -39,11 +39,7 @@ public final class FeedViewController: UITableViewController, FeedLoadingView, F
     }
     
     func display(_ viewModel: FeedLoadingViewModel) {
-        if viewModel.isLoading {
-            refreshControl?.beginRefreshing()
-        } else {
-            refreshControl?.endRefreshing()
-        }
+        refreshControl?.update(isRefreshing: viewModel.isLoading)
     }
     
     func display(_ viewModel: FeedErrorViewModel) {
