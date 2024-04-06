88a6d7628a7770766470718d202ec2310601ec88
finebel <46824694+finebel@users.noreply.github.com>
2024-04-06 18:12:26 +0200

    Move `FeedStore` conformance of `CoreDataFeedStore` to a separate file

--- EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CoreData
 
-public final class CoreDataFeedStore: FeedStore {
+public final class CoreDataFeedStore {
     private static let modelName = "FeedStore"
     private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
     
@@ -31,43 +31,6 @@ public final class CoreDataFeedStore: FeedStore {
             context = container.newBackgroundContext()
         } catch {
             throw StoreError.failedToLoadPersistentStores(error)
-        }
-    }
-    
-    public func retrieve(completion: @escaping RetrievalCompletion) {
-        perform { context in
-            let retrievalResult: FeedStore.RetrievalResult = Result {
-                try ManagedCache.find(in: context).map {
-                    CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
-                }
-            }
-            
-            completion(retrievalResult)
-        }
-    }
-    
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        perform { context in
-            let insertResult: FeedStore.InsertionResult = Result {
-                let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                managedCache.timestamp = timestamp
-                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-                
-                try context.save()
-            }
-            
-            completion(insertResult)
-        }
-    }
-    
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        perform { context in
-            let deleteResult: FeedStore.DeletionResult = Result {
-                try ManagedCache.find(in: context)
-                    .map(context.delete)
-                    .map(context.save)
-            }
-            completion(deleteResult)
         }
     }
     
