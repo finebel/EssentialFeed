b7088d70f216bf6e1b4634f73c11315b05a74444
finebel <46824694+finebel@users.noreply.github.com>
2024-04-27 16:17:45 +0200

    Check `isMainQueue` instead of just `isMainThread` since it's not guranteed that the main thread is running the main queue

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -77,7 +77,7 @@ extension Publisher {
 }
 
 extension DispatchQueue {
-    static var immediateWhenOnMainQueueScheduler = ImmediateWhenOnMainQueueScheduler()
+    static var immediateWhenOnMainQueueScheduler = ImmediateWhenOnMainQueueScheduler.shared
     
     struct ImmediateWhenOnMainQueueScheduler: Scheduler {
         typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
@@ -91,8 +91,22 @@ extension DispatchQueue {
             DispatchQueue.main.minimumTolerance
         }
         
+        static let shared = Self()
+        
+        private static let key = DispatchSpecificKey<UInt8>()
+        private static let value = UInt8.max
+        
+        // make init private and use Singleton pattern to prevent setting the value multiple times
+        private init() {
+            DispatchQueue.main.setSpecific(key: Self.key, value: Self.value)
+        }
+        
+        private func isMainQueue() -> Bool {
+            DispatchQueue.getSpecific(key: Self.key) == Self.value
+        }
+        
         func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
-            guard Thread.isMainThread else {
+            guard isMainQueue() else {
                 return DispatchQueue.main.schedule(options: options, action)
             }
             
