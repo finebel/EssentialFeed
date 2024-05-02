7312a719c572b3601acaca17825685737962e2c1
finebel <46824694+finebel@users.noreply.github.com>
2024-05-02 20:26:50 +0200

    Duplicate `FeedPresenter` as `LoadResourcePresenter`

---
+++ EssentialFeed/EssentialFeed/SharedPresentation/LoadResourcePresenter.swift
@@ -0,0 +1,54 @@
+//
+//  LoadResourcePresenter.swift
+//  EssentialFeed
+//
+//  Created by Finn Ebeling on 02.05.24.
+//
+
+import Foundation
+
+public final class LoadResourcePresenter {
+    private let feedView: FeedView
+    private let loadingView: FeedLoadingView
+    private let errorView: FeedErrorView
+    
+    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+        self.feedView = feedView
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
+    
+    public static var title: String {
+        NSLocalizedString(
+            "FEED_VIEW_TITLE",
+            tableName: "Feed",
+            bundle: Bundle(for: Self.self),
+            comment: "Title for the feed view"
+        )
+    }
+    
+    private var feedLoadErrorMessage: String {
+        NSLocalizedString(
+            "FEED_VIEW_CONNECTION_ERROR",
+            tableName: "Feed",
+            bundle: Bundle(for: Self.self),
+            comment: "Error message displayed when we can't load the image feed from the server"
+        )
+    }
+    
+    public func didStartLoadingFeed() {
+        errorView.display(.noError)
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+    
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+    
+    public func didFinishLoadingWithError(with error: Error) {
+        errorView.display(.error(message: feedLoadErrorMessage))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+
+}
