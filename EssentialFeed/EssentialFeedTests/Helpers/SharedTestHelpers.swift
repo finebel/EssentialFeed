c72acbf009ecd1a992767a4614fb9879856021c9
finebel <46824694+finebel@users.noreply.github.com>
2024-05-05 08:50:58 +0200

    Map `ImageComment`s into presentable `ImageCommentViewModel`s wrapped inside a `ImageCommentsViewModel`

--- EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -29,3 +29,17 @@ extension HTTPURLResponse {
         self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
     }
 }
+
+extension Date {
+    func adding(seconds: Int) -> Date {
+        Calendar(identifier: .gregorian).date(byAdding: .second, value: seconds, to: self)!
+    }
+    
+    func adding(minutes: Int) -> Date {
+        Calendar(identifier: .gregorian).date(byAdding: .minute, value: minutes, to: self)!
+    }
+    
+    func adding(days: Int) -> Date {
+        Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+}
