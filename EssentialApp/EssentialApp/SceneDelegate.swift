cee42d3eb87c8f17e997d3c895f0a6f8c662d163
finebel <46824694+finebel@users.noreply.github.com>
2024-05-20 16:39:00 +0200

    Fetch current items from cache when needed instead of keeping them in memory all the time

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -95,12 +95,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .eraseToAnyPublisher()
     }
     
-    private func makeRemoteLoadMoreLoader(items: [FeedImage], last: FeedImage?) -> AnyPublisher<Paginated<FeedImage>, Error> {
-        makeRemoteFeedLoader(after: last)
-                .map { newItems in (items + newItems, newItems.last) }
-                .map(makePage)
-                .caching(to: localFeedLoader)
-                .eraseToAnyPublisher()
+    private func makeRemoteLoadMoreLoader(last: FeedImage?) -> AnyPublisher<Paginated<FeedImage>, Error> {
+        localFeedLoader.loadPublisher()
+            .zip(makeRemoteFeedLoader(after: last))
+            .map { (cachedItems, newItems) in (cachedItems + newItems, newItems.last) }
+            .map(makePage)
+            .caching(to: localFeedLoader)
+            .eraseToAnyPublisher()
     }
 
     private func makeFirstPage(items: [FeedImage]) -> Paginated<FeedImage> {
@@ -110,7 +111,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private func makePage(items: [FeedImage], last: FeedImage?) -> Paginated<FeedImage> {
         Paginated(
             items: items,
-            loadMorePublisher: last == nil ? nil : { self.makeRemoteLoadMoreLoader(items: items, last: last) }
+            loadMorePublisher: last == nil ? nil : { self.makeRemoteLoadMoreLoader(last: last) }
         )
     }
     
