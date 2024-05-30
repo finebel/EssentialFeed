824ee293774e81db02ff0d7545539ca0e1705616
finebel <46824694+finebel@users.noreply.github.com>
2024-05-30 21:03:43 +0200

    Ensure all store operations run in the same scheduler
    When using `.subscribe(on: scheduler)` we change the scheduler for ALL upstream publishers. This can lead to scenarios, in which, for example, network requests are using the same scheduler we use for our store operations. To avoid this, we replace `.subscribe(on: scheduler)` with `.receive(on: scheduler)` to only affect downstream subscribers.
    
    See https://github.com/essentialdevelopercom/essential-feed-case-study/commit/952ace7603c55aa9ab5f59d417b6d500e9e86fba for reference and further information

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -79,10 +79,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func sceneWillResignActive(_ scene: UIScene) {
-        do {
-            try localFeedLoader.validateCache()
-        } catch {
-            logger.error("Failed to validate cache with error: \(error.localizedDescription)")
+        scheduler.schedule { [localFeedLoader, logger] in
+            do {
+                try localFeedLoader.validateCache()
+            } catch {
+                logger.error("Failed to validate cache with error: \(error.localizedDescription)")
+            }
         }
     }
     
@@ -113,10 +115,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<Paginated<FeedImage>, Error> {
         makeRemoteFeedLoader()
+            .receive(on: scheduler)
             .caching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)
             .map(makeFirstPage)
-            .subscribe(on: scheduler)
             .eraseToAnyPublisher()
     }
     
@@ -125,8 +127,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .zip(makeRemoteFeedLoader(after: last))
             .map { (cachedItems, newItems) in (cachedItems + newItems, newItems.last) }
             .map(makePage)
+            .receive(on: scheduler)
             .caching(to: localFeedLoader)
-            .subscribe(on: scheduler)
             .eraseToAnyPublisher()
     }
 
@@ -150,8 +152,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 httpClient
                     .getPublisher(url: url)
                     .tryMap(FeedImageDataMapper.map)
+                    .receive(on: scheduler)
                     .caching(to: localImageLoader, using: url)
-                    .subscribe(on: scheduler)
                     .eraseToAnyPublisher()
             })
             .subscribe(on: scheduler)
