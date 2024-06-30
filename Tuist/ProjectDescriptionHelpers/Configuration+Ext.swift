ec7757ffee634cb096884dec5cbf983ff263cf9c
finebel <46824694+finebel@users.noreply.github.com>
2024-06-30 13:28:40 +0200

    Specify EssentialApp signing information at the target level via tuist

--- Tuist/ProjectDescriptionHelpers/Configuration+Ext.swift
+++ Tuist/ProjectDescriptionHelpers/Configuration+Ext.swift
@@ -5,11 +5,23 @@ import ProjectDescription
 
 public extension Configuration {
     static var essentialAppDebug: Self {
-        .debug(name: "Debug", xcconfig: .relativeToRoot("ConfigFiles/EssentialApp.xcconfig"))
+        .debug(
+            name: "Debug",
+            settings: SettingsDictionary().automaticCodeSigning(devTeam: "K2H77582Z5"),
+            xcconfig: .relativeToRoot("ConfigFiles/EssentialApp.xcconfig")
+        )
     }
     
     static var essentialAppRelease: Self {
-        .release(name: "Release", xcconfig: .relativeToRoot("ConfigFiles/EssentialApp.xcconfig"))
+        .release(
+            name: "Release",
+            settings: SettingsDictionary()
+                .manualCodeSigning(
+                    identity: "Apple Distribution: Finn Ebeling (K2H77582Z5)",
+                    provisioningProfileSpecifier: "EssentialAppDistributionProfile"
+                ),
+            xcconfig: .relativeToRoot("ConfigFiles/EssentialApp.xcconfig")
+        )
     }
 }
 
